a7a2f518a4ab03f09aa0ede3203aa7a20a717ae2
arya01493 <happyaarya5@gmail.com>
2025-09-03 02:17:29 -0400

    Update app.js

--- app.js
+++ app.js
@@ -2,6 +2,6 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get('/', (_, res) => res.send('Hello from CI/CD with Jenkins + Docker + Trivy!'));
+app.get('/', (_, res) => res.send('Hello from CI/CD with Jenkins + Docker + Trivy! + Defect Dojo ' ));
 
 app.listen(port, () => console.log(`App listening on port ${port}`));
